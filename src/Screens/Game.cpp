42c5153b8911a602e0af7611dd7b99ac5c3cf6a8
MateoMonastra <mateomonastravm@gmail.com>
2023-11-15 20:30:15 -0300

    remove version from game scene

--- src/Screens/Game.cpp
+++ src/Screens/Game.cpp
@@ -48,7 +48,6 @@ namespace flappybird
 			DrawParallax();
 			BirdDraw(player);
 			ObstacleDraw(obstacle);
-			DrawText("0.2", GetScreenWidth() - 60, GetScreenHeight() - 60, 40, BLACK);
 			DrawText("Press Esc to return Menu", GetScreenWidth() - 300, 20, 20, BLACK);
 		}
 
